2f2d12482487a7bfc11760573e955c15f99ed60c
snnbyyds <snnbyyds@gmail.com>
2023-04-15 20:44:57 +0800

    enchilada: reformat device.mk

--- device.mk
+++ device.mk
@@ -25,10 +25,10 @@ AB_OTA_PARTITIONS += \
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
-	cppreopts.sh \
+    cppreopts.sh \
     update_engine \
     update_verifier \
-	update_engine_sideload
+    update_engine_sideload
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
@@ -60,12 +60,12 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # fastbootd
 PRODUCT_PACKAGES += \
-	fastbootd \
-	android.hardware.fastboot@1.0-impl-mock \
-	android.hardware.fastboot@1.0-impl-mock.recovery
+    fastbootd \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery
 
 PRODUCT_PRODUCT_PROPERTIES += \
-	ro.fastbootd.available=true
+    ro.fastbootd.available=true
 
 # Health HAL
 PRODUCT_PACKAGES += \
